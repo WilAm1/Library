8b5070ed9a939ffd13aff14f1c214a10b88d3781
Wil <wilhelmjanmapogi@gmail.com>
2021-08-31 19:41:25 +0800

    Change to for loop the populate bookdiv

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function Book(title, author, pages, haveReadIT) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.haveReadIT = haveReadIT;
+    this.finishedReading = haveReadIT;
 }
 
 function addBookToLibrary(book) {
@@ -16,19 +16,30 @@ function createBookDiv() {
 }
 
 function populateBookDiv(div, book) {
-    const titlePara = document.createElement('p');
-    const authorPara = document.createElement('p');
-    const pagesPara = document.createElement('p');
-    const haveReadITPara = document.createElement('p');
-    titlePara.textContent = `Title: ${book.title}`;
-    authorPara.textContent = `Author: ${book.title}`;
-    pagesPara.textContent = `Pages: ${book.title}`;
-    haveReadITPara.textContent = `Have read it: ${book.haveReadIT}`;
-    div.appendChild(titlePara);
-    div.appendChild(authorPara);
-    div.appendChild(pagesPara);
-    div.appendChild(haveReadITPara);
+    for (const prop in book) {
+        if (book.hasOwnProperty(prop)) {
+            const paragraphElement = document.createElement('p');
+            paragraphElement.classList.add(prop);
+            paragraphElement.textContent = (prop === 'finishedReading') ?
+                `FINISHED READING: ${book[prop]}` : `${prop.toUpperCase()}: ${book[prop]}`;
+            div.appendChild(paragraphElement);
+        }
+
+    }
+    // const titlePara = document.createElement('p');
+    // const authorPara = document.createElement('p');
+    // const pagesPara = document.createElement('p');
+    // const haveReadITPara = document.createElement('p');
+    // titlePara.textContent = `Title: ${book.title}`;
+    // authorPara.textContent = `Author: ${book.title}`;
+    // pagesPara.textContent = `Pages: ${book.title}`;
+    // haveReadITPara.textContent = `Have read it: ${book.haveReadIT}`;
+    // div.appendChild(titlePara);
+    // div.appendChild(authorPara);
+    // div.appendChild(pagesPara);
+    // div.appendChild(haveReadITPara);
 }
+
 const Book1 = new Book('fart', 'fastfary', 123, false);
 const Book2 = new Book('fart2', 'fastfary', 123, false);
 const Book3 = new Book('fart3', 'fastfary', 123, false);
